257d1cc2cfdddbc41b8ca15f438f13f6555f7acc
Bideo Wego <bideowego@gmail.com>
2016-04-17 01:26:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Travis CI Test
+
+
+
+
+
+
+
+
---
+++ lib/zombie.rb
@@ -0,0 +1,15 @@
+class Zombie
+  def initialize
+    @alive = false
+  end
+
+  def alive?
+    @alive
+  end
+
+  def undead?
+    !@alive
+  end
+end
+
+
